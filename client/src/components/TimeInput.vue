f2b3f01c215b9a7068493e1b2f395361e0e80b3b
Nenad Skocic <nenad.skocic@gmail.com>
2020-10-30 06:01:20 -0400

    selector

---
+++ client/src/components/TimeInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="number" readonly v-model="selectedValue" @focus="focused" v-bind:min="min" v-bind:max="max" v-bind:placeholder="placeholder">
+        <div v-show="fieldFocused">
+            <input type="range" @change="changed" v-bind:min="min" v-bind:max="max" v-bind:step="step">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TimeInput",
+    props: ['min', 'max', 'placeholder', 'step'],
+    methods: {
+        changed(evt) {
+            this.selectedValue = evt.target.value;
+            this.fieldFocused = false;
+            this.$emit('input', this.selectedValue);
+        },
+        focused() {
+            this.fieldFocused = true;
+        }
+    },
+    data() {
+        return {
+            fieldFocused: false,
+            selectedValue: 0,
+        }
+    }
+}
+</script>
