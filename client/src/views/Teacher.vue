ff367cc437fa883b11e60da92eefdd3def175b4d
Nenad Skocic <nenad.skocic@gmail.com>
2020-11-14 09:42:13 -0500

    check student answer

--- client/src/views/Teacher.vue
+++ client/src/views/Teacher.vue
@@ -12,16 +12,31 @@
         </select>
       </div>
       <div class="col-4">
-        <input type="number" min="0" placeholder="Score" v-model="questionScore">
+        <input
+          type="number"
+          min="0"
+          placeholder="Score"
+          v-model="questionScore"
+        />
       </div>
       <div class="col-2">
-        <TimeInput min="10" max="90" step="5" placeholder="Time" v-model="questionTime" @input="questionTime = $event" />
+        <TimeInput
+          min="10"
+          max="90"
+          step="5"
+          placeholder="Time"
+          v-model="questionTime"
+          @input="questionTime = $event"
+        />
       </div>
     </div>
 
     <div class="row">
       <div class="col-12">
-        <QuestionEntry v-bind:type="questionType" @input="questionContent = $event" />
+        <QuestionEntry
+          v-bind:type="questionType"
+          @input="questionContent = $event"
+        />
       </div>
     </div>
 
@@ -32,17 +47,45 @@
     </div>
 
     <div class="dialog" v-if="questionPosted">
-      <div class="content">
-        Results!!!111
+      <div class="content" v-if="!answerReceived">
+        <h4>Please submit your answer...</h4>
+        <div v-for="result in studentAnswer" :key="result.page">
+          <h3>Results</h3>
+          <div
+            v-if="
+              JSON.stringify(result.answerContent.answerContent) ===
+              JSON.stringify(questionContent.answers)
+            "
+          >
+            <h2>Correct answer!</h2>
+          </div>
+          <div v-else>
+            <h2 id="incorrect">Incorrect answer!</h2>
+            <p>Correct answer was:</p>
+            <div
+              v-for="originalContent in questionContent"
+              :key="originalContent.page"
+            >
+              <div v-for="incorrect in originalContent" :key="incorrect.page">
+                <div v-if="incorrect.correct === true">
+                  <li>{{ incorrect.text }}</li>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="scoreContent">
+            <p>Your current score</p>
+            <h2>{{ questionScore }}</h2>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import TimeInput from '../components/TimeInput';
-import QuestionEntry from '../components/QuestionEntry';
+import TimeInput from "../components/TimeInput";
+import QuestionEntry from "../components/QuestionEntry";
 
 export default {
   components: {
@@ -51,16 +94,20 @@ export default {
   },
   sockets: {
     connect() {
-      console.log('Teacher.vue: socket connected');
+      console.log("Teacher.vue: socket connected");
 
-      this.sockets.subscribe('student-registered', (data) => {
+      this.sockets.subscribe("student-registered", (data) => {
         this.students.push(data);
       });
-    }
+
+      this.sockets.subscribe("student-answer", (data) => {
+        this.studentAnswer.push(data);
+      });
+    },
   },
   methods: {
     poseQuestionToStudents() {
-      this.$socket.emit('teacher-new-question', {
+      this.$socket.emit("teacher-new-question", {
         questionContent: this.questionContent, // Question and answers to question
         questionTime: this.questionTime, // Time to answer question
         questionScore: this.questionScore, // Score for getting question correct
@@ -68,11 +115,22 @@ export default {
       });
       this.questionPosted = true;
     },
+    sendStudentResults() {
+      this.studentResult = true;
+
+      this.$socket.emit("teacher-marked-quiz", {
+        studentResult: this.studentResult,
+      });
+    },
   },
   computed: {
     isQuestionReady() {
-      return this.questionScore > 0 && this.questionTime > 0 && this.questionType != "";
-    }
+      return (
+        this.questionScore > 0 &&
+        this.questionTime > 0 &&
+        this.questionType != ""
+      );
+    },
   },
   data() {
     return {
@@ -82,11 +140,14 @@ export default {
       questionScore: 0,
       questionContent: null,
       students: [],
-    }
+      studentAnswer: [],
+      studentResult: false,
+      answerReceived: false,
+      correctAnswer: "",
+    };
   },
-  mounted() {
-  }
-}
+  mounted() {},
+};
 </script>
 
 <style scoped>
@@ -111,5 +172,20 @@ export default {
   box-shadow: -10px 10px 20px -10px rgba(0, 0, 0, 0.2);
   background: white;
   text-align: center;
+}
+
+#incorrect {
+  color: red;
+}
+
+.scoreContent {
+  padding: 5px;
+  width: 30%;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  border-radius: 10px;
+  box-shadow: -10px 10px 20px -10px rgba(0, 0, 0, 0.2);
+  background: rgb(214, 246, 255);
+  float: none;
+  overflow: hidden;
 }
 </style>
