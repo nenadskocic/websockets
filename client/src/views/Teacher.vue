23cf588a99172b1fa73c015f8d2285203aa58269
Nenad Skocic <nenad.skocic@gmail.com>
2020-12-11 21:54:49 -0500

    finalized MC teacher stats

--- client/src/views/Teacher.vue
+++ client/src/views/Teacher.vue
@@ -48,50 +48,55 @@
       </div>
     </div>
 
-      <div class="studentResults" v-if="questionPosted">
-        <div class="content">
-          <div class="resultsComp">
-            <p>Time remaining</p>
-            <p>{{ this.timer }}</p>
-          </div>
-          <div class="resultsComp">
-            <p>% correct</p>
-            <p>{{ this.percentageCorrect }}</p>
-          </div>
-          <div class="resultsComp">
-            <p>Total correct</p>
-            <p>{{ this.totalCorrect }}</p>
-          </div>
-          <div class="resultsComp">
-            <p>Total Answers</p>
-            <p>{{ this.totalAnswers }}</p>
-          </div>
+    <div class="studentResults" v-if="questionPosted">
+      <div class="content">
+        <div class="resultsComp">
+          <p>Time remaining</p>
+          <p>{{ this.timer }}</p>
+        </div>
+        <div class="resultsComp">
+          <p>% correct</p>
+          <p>{{ this.percentageCorrect }}</p>
+        </div>
+        <div class="resultsComp">
+          <p>Total correct</p>
+          <p>{{ this.totalCorrect }}</p>
+        </div>
+        <div class="resultsComp">
+          <p>Total Answers</p>
+          <p>{{ this.totalAnswers }}</p>
+        </div>
 
-          <div class="row" v-for="statsL in teacherStats" :key="statsL.page">
-            <div class="col-6">
-              <h2>Correct</h2>
-              <div v-if="isStudCorrect">
-                <div class="col-6">{{ statsL.teacherStats.studentName }}</div>
-                <div class="col-2">{{ statsL.teacherStats.scoreCounter }}</div>
-              </div>
-              <div v-else>
-                <div class="col-6">{{ statsL.teacherStats.studentName }}</div>
-                <div class="col-2">{{ statsL.teacherStats.scoreCounter }}</div>
+        <div class="teacherStatsTable">
+          <div>
+            <h2>Results</h2>
+          </div>
+          <div v-for="statsL in teacherStats" :key="statsL.page">
+            <div class="row">
+              <div class="col-4">
+                <div class="col-4">{{ statsL.teacherStats.studentName }}</div>
+                <div class="col-4">{{ statsL.teacherStats.scoreCounter }}</div>
+                <div v-if="statsL.teacherStats.isStudCorrect == true">
+                  <div class="col-3">Correct</div>
+                </div>
+                <div v-else>
+                  <div class="col-3">Incorrect</div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
 
-          <div class="submitAnswer" v-if="isReadyNext">
-            <button v-on:click="askAnother">Ask another question</button>
-          </div>
+        <div class="submitAnswer" v-if="isReadyNext">
+          <button v-on:click="askAnother">Ask another question</button>
         </div>
       </div>
+    </div>
   </div>
 </template>
 <script>
 import TimeInput from "../components/TimeInput";
 import QuestionEntry from "../components/QuestionEntry";
-import _ from "lodash";
 
 export default {
   components: {
@@ -105,17 +110,6 @@ export default {
       this.sockets.subscribe("student-registered", (data) => {
         this.students.push(data);
       });
-    },
-  },
-  methods: {
-    poseQuestionToStudents() {
-      this.$socket.emit("teacher-new-question", {
-        questionContent: this.questionContent,
-        questionTime: this.questionTime,
-        questionScore: this.questionScore,
-        questionType: this.questionType,
-      });
-      this.questionPosted = true;
 
       this.sockets.subscribe("teacher-result-display", (data) => {
         this.teacherStats.push(data);
@@ -127,6 +121,17 @@ export default {
         this.isStudCorrect = data.teacherStats.isStudCorrect;
         this.timer = parseInt(data.teacherStats.timer);
       });
+    },
+  },
+  methods: {
+    poseQuestionToStudents() {
+      this.$socket.emit("teacher-new-question", {
+        questionContent: this.questionContent,
+        questionTime: this.questionTime,
+        questionScore: this.questionScore,
+        questionType: this.questionType,
+      });
+      this.questionPosted = true;
     },
     askAnother() {
       this.questionPosted = false;
@@ -217,7 +222,8 @@ export default {
   margin-bottom: 5%;
 }
 
-.submitAnswer {
-  margin-top: 30%;
+.teacherStatsTable {
+  text-align: left;
+  margin-bottom: 4%;
 }
 </style>
